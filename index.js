ce1edbf2e8d66e0c9b6a7e586c0372d3dcfbeb80
minahany0000 <hhmina00@gmail.com>
2025-03-16 12:36:37 +0200

    debug

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import express from 'express'
 import dotenv from "dotenv";
 import { initApp } from './src/initApp.js';
 import cors from "cors"
+try {
+    dotenv.config();
 
-dotenv.config();
+    const app = express()
 
-const app = express()
-
-app.use(cors())
-
-
-
-
-initApp(app, express)
+    app.use(cors())
+    initApp(app, express)
+}
+catch (error) {
+    console.log(error)
+}
